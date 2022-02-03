57b571a32d302eef892e4f9518a9d3cd0548dcc7
alexandre <alexandre@pandascore.co>
2022-02-03 19:16:47 +0100

    img background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -32,7 +32,7 @@
 
 	<Slide>
 		<div
-			class="flex flex-wrap justify-around items-center mx-auto max-w-22/24 pt-20"
+			class="flex flex-wrap justify-evenly items-center mx-auto max-w-22/24 pt-20"
 		>
 			<div class="lg:w-11/24 w-22/24">
 				<p
@@ -61,13 +61,13 @@
 				</p>
 			</div>
 			<div class="lg:w-11/24 w-22/24">
-				<img src="14.svg" alt="cool" class="rounded-20" />
+				<img src="14.svg" alt="figure-1" class="rounded-20" />
 			</div>
 		</div>
 	</Slide>
 
 	<Slide>
-		<div class="flex flex-wrap justify-around mx-auto max-w-22/24 pt-20">
+		<div class="flex flex-wrap justify-evenly mx-auto max-w-22/24 pt-20">
 			<div class="xl:w-11/24 w-22/24">
 				<p
 					class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
@@ -96,7 +96,7 @@
 			</div>
 			<div class="xl:w-11/24 w-22/24">
 				<div
-					class="flex flex-wrap justify-around items-top mx-auto max-w-22/24"
+					class="flex flex-wrap justify-evenly items-top mx-auto max-w-22/24"
 				>
 					<div class="w-23/24 md:w-11/24 py-10">
 						<div class="i-openmoji-scarf text-5xl" />
@@ -156,8 +156,9 @@
 			</div>
 		</div>
 	</Slide>
+
 	<Slide>
-		<div class="flex flex-col min-h-screen pt-20 ml-8">
+		<div class="flex flex-col min-h-screen pt-15 ml-8">
 			<p
 				class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
 			>
@@ -174,9 +175,9 @@
 				Free and Premium themes, UI Kit's, templates and landing pages
 				built with Tailwind CSS, HTML & Next.js.
 			</p>
-			<div class="flex flex-wrap min-w-full justify-around">
+			<div class="flex flex-wrap min-w-full justify-evenly">
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-shooting-star text-5xl" />
 					<h2
@@ -192,7 +193,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-test-tube text-5xl" />
 					<h2
@@ -208,7 +209,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-ringed-planet text-5xl" />
 					<h2
@@ -224,7 +225,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-violin text-5xl" />
 					<h2
@@ -240,7 +241,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-radioactive text-5xl" />
 					<h2
@@ -256,7 +257,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
 				>
 					<div class="i-openmoji-shower text-5xl" />
 					<h2
@@ -271,6 +272,93 @@
 						subway tile poke farm
 					</p>
 				</div>
+			</div>
+		</div>
+	</Slide>
+
+	<Slide>
+		<div class="flex flex-col min-h-screen pt-15">
+			<p
+				class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
+			>
+				your tagline
+			</p>
+			<h1
+				class="font-sans font-black text-neutral-900 text-left text-6xl tracking-tighter mt-4 mb-8 leading-none"
+			>
+				Long headline to turn your visitors into users.
+			</h1>
+			<p
+				class="font-sans font-normal text-lg text-gray-500 max-w-5/6 my-8 leading-relaxed"
+			>
+				Free and Premium themes, UI Kit's, templates and landing pages
+				built with Tailwind CSS, HTML & Next.js.
+			</p>
+			<div
+				class="flex flex-wrap max-h-full max-w-full justify-evenly mx-6"
+			>
+				<div
+					class="flex-auto xl:w-10/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+				>
+					<img src="12.svg" class="w-5/6" />
+					<h2
+						class="font-sans font-semibold text-neutral-900 mt-6 text-2xl tracking-tighter leading-none"
+					>
+						Shooting Stars.
+					</h2>
+					<p
+						class="font-sans font-light text-gray-500 mt-4 leading-relaxed"
+					>
+						Fingerstache flexitarian street art 8-bit waist co,
+						subway tile poke farm
+					</p>
+				</div>
+				<div
+					class="flex-auto xl:w-10/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg"
+				>
+					<img src="14.svg" class="w-5/6" />
+					<h2
+						class="font-sans font-semibold text-neutral-900 mt-6 text-2xl tracking-tighter leading-none"
+					>
+						The Catalyzer.
+					</h2>
+					<p
+						class="font-sans font-light text-gray-500 mt-4 leading-relaxed"
+					>
+						Fingerstache flexitarian street art 8-bit waist co,
+						subway tile poke farm
+					</p>
+				</div>
+			</div>
+		</div>
+	</Slide>
+
+	<Slide>
+		<div class="flex flex-col min-h-screen justify-center">
+			<img
+				src="bg.jpg"
+				alt="background"
+				class="fixed object-cover w-screen h-screen"
+			/>
+			<div
+				class="absolute md:self-end lg:mr-1/8 m-5 sm:p-10 p-5 lg:w-1/3 md:w-1/2 bg-white rounded-lg relative shadow-md op90"
+			>
+				<p
+					class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
+				>
+					your tagline
+				</p>
+				<h1
+					class="font-sans font-black text-neutral-900 text-5xl text-left mt-8 tracking-tighter"
+				>
+					Medium length display headline.
+				</h1>
+				<p
+					class="font-sans font-light text-xl text-gray-500 mt-8 leading-relaxed"
+				>
+					Free and Premium themes, UI Kit's, templates and landing
+					pages built with Tailwind CSS, HTML & Next.js.
+				</p>
 			</div>
 		</div>
 	</Slide>
