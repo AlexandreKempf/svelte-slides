ad2489be183f11193c29cede0c116d7a7a77d8d1
alexandre <alexandre@pandascore.co>
2022-02-05 21:50:06 +0100

    add slides

--- src/lib/Slide.svelte
+++ src/lib/Slide.svelte
@@ -14,31 +14,39 @@
 
 	let Slide;
 
-	const addClassAtStep = (container, step) => {
+	const updateClassAtStep = (container, step) => {
 		const children = container.getElementsByTagName("*");
-		const prefix = "step-" + step + ":";
+		const prefix_start = "start-" + step + ":";
+		const prefix_end = "end-" + step + ":";
 		for (var i = 0; i < children.length; i++) {
 			const child = children[i];
 			const classList = child.classList;
 			for (var j = 0; j < classList.length; j++) {
-				if (classList[j].startsWith("step")) {
+				if (classList[j].startsWith("start")) {
 					if (step == 0) {
-						//reset the slide to step-0
+						//reset the slide to step-0 by removing all step-*: classes added
 						const classToRemove = classList[j]
 							.split(":")
 							.slice(1)
 							.join("");
 						child.classList.remove(classToRemove);
-					} else if (classList[j].startsWith(prefix)) {
-						const classToAdd = classList[j].replace(prefix, "");
-						child.classList.add(classToAdd);
+					} else if (classList[j].startsWith(prefix_start)) {
+						const classToUpdate = classList[j].replace(
+							prefix_start,
+							""
+						);
+						if (child.classList.contains(classToUpdate)) {
+							child.classList.remove(classToUpdate);
+						} else {
+							child.classList.add(classToUpdate);
+						}
 					}
 				}
 			}
 		}
 	};
 
-	$: Slide && $currentIndex == slideIndex && addClassAtStep(Slide, $step); // add class at a specific step or reset if step==0
+	$: Slide && $currentIndex == slideIndex && updateClassAtStep(Slide, $step); // update class at a specific step or reset if step==0
 </script>
 
 <div class:hidden bind:this={Slide} class="overflow-x-hidden">
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -32,38 +32,36 @@
 		<div
 			class="flex flex-wrap justify-evenly items-center mx-auto max-w-22/24 pt-20"
 		>
-			<div class="lg:w-11/24 w-22/24">
-				<div class="animate__animated animate__fadeInLeft">
-					<img src="14.svg" alt="figure-1" class="rounded-20" />
-				</div>
+			<div
+				class="lg:w-11/24 w-22/24 animate__animated animate__fadeInLeft"
+			>
+				<img src="14.svg" alt="figure-1" class="rounded-20" />
 			</div>
-			<div class="lg:w-11/24 w-22/24">
-				<div class="animate__animated animate__fadeInUp">
-					<p
-						class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
-					>
-						your tagline
-					</p>
-					<h1
-						class="font-sans font-black text-neutral-900 text-5xl text-left mt-8 tracking-tighter"
-					>
-						Medium length display headline.
-					</h1>
-					<p
-						class="font-sans font-light text-xl text-gray-500 mt-8 leading-relaxed"
-					>
-						Free and Premium themes, UI Kit's, templates and landing
-						pages built with Tailwind CSS, HTML & Next.js.
-					</p>
+			<div class="lg:w-11/24 w-22/24 animate__animated animate__fadeInUp">
+				<p
+					class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
+				>
+					your tagline
+				</p>
+				<h1
+					class="font-sans font-black text-neutral-900 text-5xl text-left mt-8 tracking-tighter"
+				>
+					Medium length display headline.
+				</h1>
+				<p
+					class="font-sans font-light text-xl text-gray-500 mt-8 leading-relaxed"
+				>
+					Free and Premium themes, UI Kit's, templates and landing
+					pages built with Tailwind CSS, HTML & Next.js.
+				</p>
 
-					<p
-						class="font-sans font-light text-xl text-gray-500 mt-8 leading-relaxed"
-					>
-						Ain't no sunshine when she's gone. <br />
-						Expensive feature away. <br />
-						Really good features.
-					</p>
-				</div>
+				<p
+					class="font-sans font-light text-xl text-gray-500 mt-8 leading-relaxed"
+				>
+					Ain't no sunshine when she's gone. <br />
+					Expensive feature away. <br />
+					Really good features.
+				</p>
 			</div>
 		</div>
 	</Slide>
@@ -143,7 +141,7 @@
 					class="flex flex-wrap justify-evenly items-top mx-auto max-w-22/24"
 				>
 					<div
-						class="w-23/24 md:w-11/24 py-10 animate__animated op0 step-1:op100 step-1:animate__fadeInRight"
+						class="w-23/24 md:w-11/24 py-10 animate__animated op0 start-1:op100 start-1:animate__fadeInRight"
 					>
 						<div class="i-openmoji-scarf text-5xl" />
 						<h2
@@ -158,7 +156,7 @@
 						</p>
 					</div>
 					<div
-						class="w-23/24 md:w-11/24 py-10 animate__animated op0 step-2:op100 step-2:animate__fadeInRight"
+						class="w-23/24 md:w-11/24 py-10 animate__animated op0 start-2:op100 start-2:animate__fadeInRight"
 					>
 						<div class="i-openmoji-rocket text-5xl" />
 						<h2
@@ -175,7 +173,7 @@
 						</p>
 					</div>
 					<div
-						class="w-23/24 md:w-11/24 py-10 animate__animated op0 step-3:op100 step-3:animate__fadeInRight"
+						class="w-23/24 md:w-11/24 py-10 animate__animated op0 start-3:op100 start-3:animate__fadeInRight"
 					>
 						<div class="i-openmoji-bullseye text-5xl" />
 						<h2
@@ -190,7 +188,7 @@
 						</p>
 					</div>
 					<div
-						class="w-23/24 md:w-11/24 py-10 animate__animated op0 step-4:op100 step-4:animate__fadeInRight"
+						class="w-23/24 md:w-11/24 py-10 animate__animated op0 start-4:op100 start-4:animate__fadeInRight"
 					>
 						<div class="i-openmoji-telephone text-5xl" />
 						<h2
@@ -209,7 +207,7 @@
 		</div>
 	</Slide>
 
-	<Slide>
+	<Slide maxSteps={6}>
 		<div class="flex flex-col min-h-screen pt-15 ml-8">
 			<p
 				class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
@@ -230,8 +228,9 @@
 			<div
 				class="flex flex-wrap max-h-full max-w-full justify-evenly mx-6"
 			>
+				<!-- bg-gray-100 -->
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-1:animate__pulse start-1:bg-gray-100"
 				>
 					<div class="i-openmoji-shooting-star text-5xl" />
 					<h2
@@ -247,7 +246,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-2:animate__pulse start-2:bg-gray-100"
 				>
 					<div class="i-openmoji-test-tube text-5xl" />
 					<h2
@@ -263,7 +262,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-3:animate__pulse start-3:bg-gray-100"
 				>
 					<div class="i-openmoji-ringed-planet text-5xl" />
 					<h2
@@ -279,7 +278,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-4:animate__pulse start-4:bg-gray-100"
 				>
 					<div class="i-openmoji-violin text-5xl" />
 					<h2
@@ -295,7 +294,7 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-5:animate__pulse start-5:bg-gray-100"
 				>
 					<div class="i-openmoji-radioactive text-5xl" />
 					<h2
@@ -311,9 +310,9 @@
 					</p>
 				</div>
 				<div
-					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md"
+					class="xl:w-11/36 md:w-8/18 border border-gray-200 p-6 m-4 ml-0 rounded-lg shadow-md animate__animated start-6:animate__pulse start-6:bg-gray-100"
 				>
-					<div class="i-openmoji-text-5xl" />
+					<div class="i-openmoji-shower text-5xl" />
 					<h2
 						class="font-sans font-semibold text-neutral-900 mt-6 text-2xl tracking-tighter leading-none"
 					>
@@ -387,7 +386,7 @@
 		</div>
 	</Slide>
 
-	<Slide>
+	<Slide maxSteps={1}>
 		<div class="flex flex-col min-h-screen justify-center">
 			<img
 				src="bg.jpg"
@@ -395,7 +394,7 @@
 				class="fixed object-cover w-screen h-screen"
 			/>
 			<div
-				class="absolute md:self-end lg:mr-1/8 m-5 sm:p-10 p-5 lg:w-1/3 md:w-1/2 bg-white rounded-lg relative shadow-md op90"
+				class="absolute md:self-end lg:mr-1/8 m-5 sm:p-10 p-5 lg:w-1/3 md:w-1/2 bg-white rounded-lg relative shadow-md op90 animate__animated animate__slideInRight"
 			>
 				<p
 					class="font-sans font-bold text-sm text-blue-600 tracking-widest uppercase"
